67bc65b4806a34a076db081aca4627d707c527f0
Zhang1506900502 <1506900502@qq.com>
2021-04-04 12:35:43 +0800

    第一次提交

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">1111</div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  mounted() {
+    function sum(x) {
+      return function (y) {
+        return x + y;
+      };
+    }
+    var sum1 = sum(1);
+    var sum10 = sum(10);
+
+    console.log(sum1(2));
+    console.log(sum10(5));
+  },
+};
+</script>
